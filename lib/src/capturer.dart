326725a269781a7c8bbf626e1404e894a48b0140
tulioccalazans <tulioccalazans@gmail.com>
2025-09-10 22:09:33 -0300

    Changing ffmpeg_kit_https_flutter (discontinued) to ffmpeg_kit_flutter_new_https_gpl

--- lib/src/capturer.dart
+++ lib/src/capturer.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
 
-import 'package:ffmpeg_kit_https_flutter/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter_new_https_gpl/ffmpeg_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:render/src/service/notifier.dart';
--- lib/src/process.dart
+++ lib/src/process.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 
-import 'package:ffmpeg_kit_https_flutter/ffmpeg_kit_config.dart';
-import 'package:ffmpeg_kit_https_flutter/ffmpeg_session.dart';
-import 'package:ffmpeg_kit_https_flutter/log.dart';
-import 'package:ffmpeg_kit_https_flutter/statistics.dart';
+import 'package:ffmpeg_kit_flutter_new_https_gpl/ffmpeg_kit_config.dart';
+import 'package:ffmpeg_kit_flutter_new_https_gpl/ffmpeg_session.dart';
+import 'package:ffmpeg_kit_flutter_new_https_gpl/log.dart';
+import 'package:ffmpeg_kit_flutter_new_https_gpl/statistics.dart';
 import 'package:render/src/formats/abstract.dart';
 import 'package:render/src/service/notifier.dart';
 import 'package:render/src/service/session.dart';
